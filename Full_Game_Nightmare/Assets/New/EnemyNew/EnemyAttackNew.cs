76770beffab53c9adf76092dcee32c75bc275a70
carlston10 <37837632+carlston10@users.noreply.github.com>
2018-04-03 01:48:12 +0200

    PlayerHealth
    -Zombunnies attack the player
    -Player Health goes down and player can die

--- Full_Game_Nightmare/Assets/New/EnemyNew/EnemyAttackNew.cs
+++ Full_Game_Nightmare/Assets/New/EnemyNew/EnemyAttackNew.cs
@@ -1,16 +1,72 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-public class EnemyAttackNew : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+public class EnemyAttackNew : MonoBehaviour
+{
+	public float timeBetweenAttacks = 0.5f;     
+	public int attackDamage = 10;               
+
+
+	Animator anim;                              
+	GameObject player;                          
+	PlayerHealthNew playerHealthNew;                  
+	//EnemyHealth enemyHealth;                    
+	bool playerInRange;                        
+	float timer;                                
+
+
+	void Awake ()
+	{
 		
+		player = GameObject.FindGameObjectWithTag ("Player");
+		playerHealthNew = player.GetComponent <PlayerHealthNew> ();
+		//enemyHealth = GetComponent<EnemyHealth>();
+		anim = GetComponent <Animator> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+
+	void OnTriggerEnter (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = true;
+		}
+	}
+
+
+	void OnTriggerExit (Collider other)
+	{
+		if(other.gameObject == player)
+		{
+			playerInRange = false;
+		}
+	}
+
+
+	void Update ()
+	{
+		timer += Time.deltaTime;
+
+		if(timer >= timeBetweenAttacks && playerInRange /*&& enemyHealth.currentHealth > 0*/)
+		{
+			Attack ();
+		}
+			
+		if(playerHealthNew.currentHealth <= 0)
+		{
+			anim.SetTrigger ("PlayerDead");
+		}
+	}
+
+
+	void Attack ()
+	{
+		timer = 0f;
+
+		if(playerHealthNew.currentHealth > 0)
+		{
+			playerHealthNew.TakeDamage (attackDamage);
+		}
 	}
 }
